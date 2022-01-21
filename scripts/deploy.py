4c0b297e9bf6966941db3fa57ef0d79d65c32bcc
Mikael <dev.kov.mtl@gmail.com>
2022-01-21 10:58:29 -0500

    added function deploy_mocks if not already deploye if we are not on chain

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,9 +1,15 @@
-from scripts.helpful_scripts import get_account
-from brownie import Lottery, accounts
+from brownie import Lottery
+from scripts.helpful_scripts import get_account, get_contract
 
 
 def deploy_lottery():
     account = get_account()
+    #  Our lottery has constructor args:
+    # address _priceFeedAddress, address _vrfCoordinator, address _link, uint256 _fee, bytes32 _keyhash
+    # if we are not on chain we need to deploy our mocks
+    # we pass contract_name from our brownie config file 
+    # from the name we get the type contract
+    lottery = Lottery.deploy(get_contract("eth_usd_price_feed"), {"from":account})
 
 
 def main():
--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -1,7 +1,13 @@
-from brownie import network, accounts, config
+from brownie import MockV3Aggregator, network, accounts, config
+from web3 import Web3
 
+# dont need to deploy a mock price feed on fork local
 FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-for-dev"]
 LOCAL_BLOCKCHAIN_ENVIRONMENTS = ["development", "ganache-local"]
+
+
+DECIMALS = 18
+INITIAL_VALUE = Web3.toWei(2000, "ether")
 
 
 # def get_account():
@@ -28,3 +34,38 @@ def get_account(index=None, id=None):
     ):
         return accounts[0]
     return accounts.add(config["wallets"]["from_key"])
+
+
+# mapping to contract_name => type_of_contract
+contract_to_mock = {"eth_usd_price_feed": MockV3Aggregator}
+
+# check if we are on chain or not
+# if not we need to pass our mock
+# from the name pass we will know type of contract
+def get_contract(contract_name):
+    """This function will grab the contract addresses from the brownie config
+    if defined, otherwise, it will deploy a mock version of that contract, otherwise  return that mock contract.
+
+        Args:
+            contract_name (string)
+
+        Returns:
+            brownie.network.contract.ProjectContract: The most recently deployed version of this contract.
+    """
+    contract_type = contract_to_mock[contract_name]
+    # check if we have to deploy a mock
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        # check to see if contract already deploy
+        if len(contract_type) <= 0:
+            deploy_mocks()
+
+
+# Deploy mock v3
+def deploy_mocks():
+    print(f"The active network is {network.show_active()}")
+    print("Deploying mocks...")
+    account = get_account()
+    mock_price_feed = MockV3Aggregator.deploy(
+        DECIMALS, INITIAL_VALUE, {"from": account}
+    )
+    print("Mocks deployed!")
